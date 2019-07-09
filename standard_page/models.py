2bfa777f057bd6fcf30e4cfdefb49180136b19f6
Mirek Zvolský <zvolsky@seznam.cz>
2019-07-09 14:48:18 +0200

    all _en fields (except of title_en) moved to extra tab

--- standard_page/models.py
+++ standard_page/models.py
@@ -1,8 +1,9 @@
-from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.admin.edit_handlers import StreamFieldPanel, TabbedInterface, ObjectList
 from wagtail.core import blocks
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
 from wagtail.images.blocks import ImageChooserBlock
+from wagtail.utils.decorators import cached_classmethod
 
 
 class StandardPage(Page):
@@ -13,5 +14,20 @@ class StandardPage(Page):
     ])
 
     content_panels = Page.content_panels + [
-        StreamFieldPanel('body'),
+        StreamFieldPanel('body_cs'),
     ]
+
+    content_panels_en = [
+        StreamFieldPanel('body_en'),
+    ]
+
+    # https://stackoverflow.com/questions/41668167/what-is-the-correct-way-to-extend-wagtail-abstract-models-with-extra-fields
+    # https://stackoverflow.com/questions/1817183/using-super-with-a-class-method
+    # https://github.com/wagtail/wagtail/blob/master/wagtail/admin/edit_handlers.py
+    @cached_classmethod
+    def get_edit_handler(cls):
+        edit_handler = super().get_edit_handler()
+        tabs = edit_handler.clone_kwargs()['children']
+        tabs.insert(1, ObjectList(cls.content_panels_en, heading='En'))
+        edit_handler = TabbedInterface(tabs, base_form_class=cls.base_form_class)
+        return edit_handler.bind_to(model=cls)
